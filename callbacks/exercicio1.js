8c3b0866248cd0693427fd0e08461bf90cf2bcdb
Victor Carvalho <hugo.vco.dev@gmail.com>
2021-09-29 19:39:30 -0300

    atualizando o exercicio1

--- callbacks/exercicio1.js
+++ callbacks/exercicio1.js
@@ -3,5 +3,5 @@ let iniciais = (nome, sobrenome) => nome[0] + sobrenome[0];
 
 let saudar = (nome, sobrenome, operacao) => "Olá " + operacao(nome, sobrenome)
 
-console.log(saudar("Victor", "Hugo", iniciais));
+console.log(saudar("Victor", "Carvalho", iniciais));
 
